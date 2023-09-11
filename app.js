7048fd4d6e2c286308d8d0932b56e637c45ba087
Bruno <delcantobruno5@gmail.com>
2023-09-11 13:41:04 -0300

    Pequeños cambios

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');
+require('ejs');
 require('dotenv').config();
 
 const app = express()
@@ -13,21 +15,16 @@ app.use(cors())
 app.use(morgan("dev"))
 app.use(express.json()) //Para que el servidor pueda comprender datos JSON
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+//Archivos estaticos
 
-app.post('/user', (req, res) => {
-    //Recibir datos por body
+app.use (express.static(path.join(__dirname, 'public')))
 
-     const {name,lastname,id} = req.body
-    //  res.send(`Bienvenido ${name} ${lastname}`)
-    res.send({
-       name,
-       lastname,
-       id
-    })
+//Motor de plantillas
+app.set('view engine', 'ejs')
 
-  })
+//Rutas
+app.use(require('./routes/blog.routes'))
+app.use(require('./routes/user.routes'))
+
 
 app.listen(port, () =>(`Servidor en http://localhost:${port}`))
